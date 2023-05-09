93933861af775b63a45c5588e511f7e9719eb59f
rudebacon <joshhan09@gmail.com>
2023-05-08 22:31:08 -0700

    progress

--- gunGame.py
+++ gunGame.py
@@ -1,10 +1,12 @@
 import cv2
 import mediapipe as mp
+import keyboard
 
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 
 cap = cv2.VideoCapture(0)  # Use the default camera (usually the built-in webcam)
+cv2.namedWindow('Hand Tracking', cv2.WINDOW_AUTOSIZE) # Create window
 
 with mp_hands.Hands(
     max_num_hands=2,  # Detect up to 2 hands in the frame
@@ -16,23 +18,41 @@ with mp_hands.Hands(
         if not success:
             print("Ignoring empty camera frame.")
             continue
-
+        image_height, image_width, _ = image.shape
+        # PROCESSING
         # Convert the BGR image to RGB and process it with Mediapipe
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         results = hands.process(image)
-
         # Draw the hand landmarks and connections on the image
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # Convert back to BGR for OpenCV
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
+
+                print('hand_landmarks:', hand_landmarks)
+                print(
+                    f'Index finger tip coordinates: (',
+                    f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
+                    f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].z * image_width}, '
+                    f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
+                )
+
                 mp_drawing.draw_landmarks(
                     image, hand_landmarks, mp_hands.HAND_CONNECTIONS
                 )
 
-        # Show the image with hand landmarks on the screen
-        cv2.imshow("Hand Tracking", image)
-        if cv2.waitKey(5) & 0xFF == 27:  # Exit if the user presses the "Esc" key
+        # cv2.waitKey(1)
+
+        # If window closed, exit (This has to be before showing image, I'm not sure why)
+        if cv2.getWindowProperty('Hand Tracking', cv2.WND_PROP_VISIBLE) < 1:
             break
+
+        # Show image
+        cv2.imshow("Hand Tracking", image)
+
+        cv2.waitKey(1)
+
+        if keyboard.is_pressed("esc"): # break if esc key pressed during playback
+            break   
 
 cap.release()
 cv2.destroyAllWindows()
